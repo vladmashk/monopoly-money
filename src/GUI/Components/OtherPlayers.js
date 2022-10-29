e94353cc9f497b461719444458e7b52ba1e7c20c
Vlad Mashkautsan <vladik.mashk@gmail.com>
2022-10-29 17:09:50 +0200

    Sort Other players by money descending

--- src/GUI/Components/OtherPlayers.js
+++ src/GUI/Components/OtherPlayers.js
@@ -3,11 +3,20 @@ import PropTypes from "prop-types";
 import {formatMoney} from "../../index.js";
 
 function OtherPlayers(props) {
+
+    function compareFunction(name1, name2) {
+        return props.state[name2] - props.state[name1]
+    }
+
     return (
         <div className="box">
             <b className="boxTitle">Other players</b>
-            {Object.keys(props.state).filter(n => n !== "Bank")
-                .map(n => <span className="otherPlayer" key={n}><span>{n}</span><span>{formatMoney(props.state[n])}</span></span>)}
+            {[...Object.keys(props.state)].filter(n => n !== "Bank").sort(compareFunction).map(n =>
+                <span className="otherPlayer" key={n}>
+                    <span>{n}</span>
+                    <span>{formatMoney(props.state[n])}</span>
+                </span>
+            )}
         </div>
     );
 }
